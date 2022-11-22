3784e8f9d6184c331efad6ff623359714a93a137
Nguyen Xuan Khoa <70152642+bill-0808@users.noreply.github.com>
2022-11-22 09:11:46 +0700

    update new db

--- src/database/connection.js
+++ src/database/connection.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
    * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
    * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
    */
-    const uri = "mongodb+srv://root:%40Bc123456789@cluster0.blsf3dp.mongodb.net/TLCN";
+    const uri = "mongodb+srv://root:%40Bc123456789@cluster0.rmn5q0o.mongodb.net/TLCN";
 
     try {
         // Connect to the MongoDB cluster
