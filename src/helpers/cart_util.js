6990aead1d3e29ce51f62fd7c96197def1de69ed
Nguyen Xuan Khoa <70152642+bill-0808@users.noreply.github.com>
2022-12-09 12:13:51 +0700

    Update add to cart

--- src/helpers/cart_util.js
+++ src/helpers/cart_util.js
@@ -1,7 +1,9 @@
 const carts = require('../models/carts.model');
 
 async function doesProductInCart(productId, userId, size) {
-    const cart = await carts.find({ user_id: userId });
+    const cart = await carts.find({ account_id: userId, status: 1 });
+
+    console.log(cart);
     for (let i = 0; i < cart.length; i++) {
         if (cart[i].product_id == productId && cart[i].size == size && cart[i].status == 1) {
             return cart[i].quantity;
